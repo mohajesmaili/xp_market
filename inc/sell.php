8b7f116b95c7cbc2f928b12e482cbb3bcd7d1323
mohammad <mo.hajesmaili@gmail.com>
2018-08-17 13:47:11 +0430

    Negative count for product

--- inc/sell.php
+++ inc/sell.php
@@ -6,10 +6,17 @@ session_start();
 
 $user_id = $_SESSION["user_id"];
 
+$result2=mysqli_query($sql,"SELECT * FROM `basket` WHERE user_id='$user_id' and sell=0");
+
+while($row=mysqli_fetch_assoc($result2)) {
+
+    $pid = $row["product_id"];
+    mysqli_query($sql, "UPDATE `product` SET `number`=`number` - 1  WHERE `id`='$pid'");
+}
+
 $update = "UPDATE `basket` SET `sell`=1  WHERE user_id='$user_id'";
 
 $result = mysqli_query($sql, $update);
-
 
 if($result){
     echo "<script>alert('خرید انجام گرفت');
